f1f2700d7aa7d4040e53ae8d1eab5d545a4f56c9
Ivan Efimov <72411972+dreamyRU@users.noreply.github.com>
2021-12-24 16:23:13 +0300

    Add files via upload

---
+++ MD22_Intent_filters/app/src/main/java/com/example/intentfilters/TimeActivity.java
@@ -0,0 +1,25 @@
+package com.example.intentfilters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+        Log.d("Activity start", "Запустилась TimeActivity");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String time = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvTime = (TextView) findViewById(R.id.tvTime);
+        tvTime.setText(time);
+    }
+}
